8ae63339cf3f378d800900bb2b0c333aee9d2b80
Emerson Matson <emersonn@cs.washington.edu>
2015-09-28 10:05:45 -0700

    changed directory name/added more metadata

--- setup.py
+++ setup.py
@@ -9,18 +9,13 @@ setup(
     author='Emerson Matson',
     author_email='emersonn@uw.edu',
     url='',
-    packages=['league'],
+    packages=['leaguepy'],
     package_data={},
     include_package_data=True,
     zip_safe=False,
     install_requires=[
         'requests',
     ],
-    entry_points={
-        'console_scripts': [
-            'bamboo-cli = bamboo.cli:main'
-        ]
-    },
     extras_require={
         'test': [
             'mock',
